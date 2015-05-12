30d966eece41799167dcdd04db8cbaae65065495
Michael Wender <michael@michaelwender.com>
2015-05-12 11:26:29 -0400

    Parsing `location` and `id` attrs via preg_match()

--- lib/classes/shortcodes.php
+++ lib/classes/shortcodes.php
@@ -30,20 +30,19 @@ class DMShortcodes extends DonationManager {
 			$regex_pattern = get_shortcode_regex();
 			preg_match ( '/'.$regex_pattern.'/s', $post->post_content, $regex_matches );
 			if( $regex_matches[2] == 'organization-seo-page' ){
-				//  Turn the attributes into a URL parm string
-	            $attribureStr = str_replace (" ", "&", trim ($regex_matches[3]));
-	            $attribureStr = str_replace ('"', '', $attribureStr);
-	            //return $attribureStr;
-	            $defaults = array(
-	            	'id' => null,
-	            	'location' => null,
-	        	);
-	            $attributes = wp_parse_args( $attribureStr, $defaults );
+				//  Parse the `id` and `location` attributes from the shortcode
+				preg_match( '/id=[\"\']?([0-9]+)[\"\']?/', $regex_matches[3], $matches );
+				if( $matches )
+					$id = $matches[1];
 
-	            if( isset( $attributes['location'] ) && ! is_null( $attributes['location'] ) ){
-	            	$organization = get_the_title( $attributes['id'] );
+				preg_match( '/location=[\"\']{1}(.*)[\"\']{1}/U', $regex_matches[3], $matches );
+				if( $matches )
+					$location = $matches[1];
+
+	            if( isset( $location ) && isset( $id ) ){
+	            	$organization = get_the_title( $id );
 	            	$format = 'Looking for a donation pick up provider in the %1$s area? Look no further...%2$s picks up donations in the following %1$s area Zip Codes.';
-	            	$description = sprintf( $format, $attributes['location'], $organization );
+	            	$description = sprintf( $format, $location, $organization );
 	            }
 			}
 		}

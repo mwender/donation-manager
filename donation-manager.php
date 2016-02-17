a27bf05441c754af881be53cf205e0f8dac75637
Michael Wender <michael@michaelwender.com>
2016-02-17 09:52:07 -0500

    Priority Option note

--- donation-manager.php
+++ donation-manager.php
@@ -824,6 +824,9 @@ class DonationManager {
                 if( ! empty( $note ) )
                     $step_one_note = $note;
 
+                if( true == $_SESSION['donor']['priority'] )
+                    $step_one_note = '<div class="alert alert-success"><strong>PRIORITY PICK UP OPTION:</strong> You have choosen our priority pick up option. Your donation will be routed to our <em>fee-based</em> provider(s) in your area.</div>' . $step_one_note;
+
                 $donation_options = array();
                 foreach( $terms as $term ) {
                     $ID = $term['id'];

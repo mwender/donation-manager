3142b1c476a93da1f5261c762bc469d89e04b3c8
Michael Wender <michael@michaelwender.com>
2018-04-27 15:53:50 -0400

    Adjusting `donor-email` image size

--- lib/fns/image-sizes.php
+++ lib/fns/image-sizes.php
@@ -3,6 +3,6 @@
 namespace DonationManager\imagesizes;
 
 function add_image_sizes(){
-    add_image_size( 'donor-email', 400 ); // 400px wide and unlimited height
+    add_image_size( 'donor-email', 600 ); // 400px wide and unlimited height
 }
 add_action( 'after_setup_theme', __NAMESPACE__ . '\\add_image_sizes' );

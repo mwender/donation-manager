f76a4d5d93a04b591a6a897644566254407a4e2d
Michael Wender <michael@michaelwender.com>
2021-01-22 16:36:25 -0500

    Updating debugger

--- lib/fns/debug.php
+++ lib/fns/debug.php
@@ -6,8 +6,9 @@ if ( !function_exists( 'write_log' ) ) {
      * @param      mixed  $log    Array or string to log.
      * @param      string $label  Label applied to the log output.
      */
-    function write_log( $log, $label = null ) {
+    function write_log( $message, $label = null ) {
         if ( true === WP_DEBUG ) {
+            /*
             if( ! is_null( $label ) )
                 $label = ':' . $label;
             $label = '[DM' . $label . '] ';
@@ -16,6 +17,16 @@ if ( !function_exists( 'write_log' ) ) {
             } else {
                 error_log( $label . $log );
             }
+            */
+          static $counter = 1;
+
+          $bt = debug_backtrace();
+          $caller = array_shift( $bt );
+
+          if( 1 == $counter )
+            error_log( "\n\n" . str_repeat('-', 25 ) . ' STARTING DEBUG [' . date('h:i:sa', current_time('timestamp') ) . '] ' . str_repeat('-', 25 ) . "\n\n" );
+          error_log( "\n" . $counter . '. ' . basename( $caller['file'] ) . '::' . $caller['line'] . "\n" . $message . "\n---\n" );
+          $counter++;
         }
     }
 }
